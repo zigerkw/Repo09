9299f977f4e2370a54d8aa09589daf7bb5ae2a05
zigerkw <zejdhadzic@hotmail.com>
2021-09-21 17:53:35 +0300

    deveta vjezba

---
+++ ex09/src/index.js
@@ -0,0 +1,18 @@
+// Only change code below this line 
+function functionElse(num) {
+    var result = "";
+    if (num <= 5) {
+        result = "5 or smaller";
+    } else { 
+        result = "Bigger than 5";
+    }
+        return result;
+}
+
+console.log(functionElse(4));
+console.log(functionElse(5));
+console.log(functionElse(6));
+console.log(functionElse(10));
+
+// Only change code above this line
+module.experts = functionElse;
