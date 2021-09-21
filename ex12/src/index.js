5767e26abef8252902b3683b8aec9241d64f487a
zigerkw <zejdhadzic@hotmail.com>
2021-09-21 19:42:41 +0300

    dvanaesta vjezba

---
+++ ex12/src/index.js
@@ -0,0 +1,26 @@
+function switchCaseSequential(num) {
+    // Write code below this line 
+    var answer = "";
+    switch (num) {
+        case 1:
+        case 2:
+        case 3:
+            answer = "Low";
+            break;
+        case 4:
+        case 5:
+        case 6:
+            answer = "Mid";
+            break;
+        case 7:
+        case 8:
+        case 9:
+            answer = "High";
+            break;
+    } 
+    // Write code above this line
+    return answer;
+}
+
+console.log(switchCaseSequential(8)); // Change this line
+module.experts = switchCaseSequential;
