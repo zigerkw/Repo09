adfe112ccd342d1761f07092bcbdb14b6b8d6022
zigerkw <zejdhadzic@hotmail.com>
2021-09-21 15:03:07 +0300

    prva vjezba

---
+++ ex01/src/index.js
@@ -0,0 +1,8 @@
+// Only change code below this line
+function myTrueBoolean() {
+    return true;
+}
+myTrueBoolean();
+console.log(myTrueBoolean());
+// Only change code above this line
+module.exports = myTrueBoolean;
